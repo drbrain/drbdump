4e27b4d1e5fa65765e841a3a47446c2093d9232b
Eric Hodel <drbrain@segment7.net>
2013-05-16 13:43:36 -0700

    Document DRbDump::TestCase

--- lib/drbdump/test_case.rb
+++ lib/drbdump/test_case.rb
@@ -2,16 +2,46 @@ require 'minitest/autorun'
 require 'drbdump'
 require 'tempfile'
 
+##
+# A test case for writing DRbDump tests.
+
 class DRbDump::TestCase < MiniTest::Unit::TestCase
 
   test = File.expand_path '../../../test', __FILE__
 
+  ##
+  # Dump containing DRb messages with arguments
+
   ARG_DUMP       = File.join test, 'arg.dump'
+
+  ##
+  # Dump containing a packet with a FIN flag
+
   FIN_DUMP       = File.join test, 'drb_fin.dump'
+
+  ##
+  # Dump containing HTTP packets
+
   HTTP_DUMP      = File.join test, 'http.dump'
+
+  ##
+  # Dump containing messages from example/ping.rb
+
   PING_DUMP      = File.join test, 'ping.dump'
+
+  ##
+  # Dump containing Rinda::RingFinger lookups
+
   RING_DUMP      = File.join test, 'ring.dump'
+
+  ##
+  # Dump containing a DRb message that is too large
+
   TOO_LARGE_DUMP = File.join test, 'too_large_packet.pcap'
+
+  ##
+  # Creates a new drbdump for +file+ and makes it available as @drbdump.
+  # Calling this again will create a brand new instance.
 
   def drbdump file = PING_DUMP
     @drbdump = DRbDump.new devices: [file]
@@ -22,9 +52,15 @@ class DRbDump::TestCase < MiniTest::Unit::TestCase
     @drbdump
   end
 
+  ##
+  # Returns a Capp packet Enumerator for +file+
+
   def packets file
     Capp.open(file).loop
   end
+
+  ##
+  # Creates a resolver for addresses in *_DUMP files
 
   def resolver
     Tempfile.open 'hosts' do |io|
