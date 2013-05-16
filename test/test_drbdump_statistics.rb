ec6d4e0bd65dc3cb58f3944955a34d87fff95a65
Eric Hodel <drbrain@segment7.net>
2013-05-16 16:11:30 -0700

    Test message and result processing

--- test/test_drbdump_statistics.rb
+++ test/test_drbdump_statistics.rb
@@ -2,6 +2,47 @@ require 'drbdump/test_case'
 
 class TestDRbDumpStatistics < DRbDump::TestCase
 
+  def setup
+    super
+
+    @MS = Marshal::Structure
+
+    @statistics = DRbDump::Statistics.new
+  end
+
+  def test_add_message_send
+    receiver = @MS.new "\x04\x080"
+    message  = @MS.new "\x04\x08\"\x0emessage"
+    argv = [
+      @MS.new("\x04\x08i\x00"),
+      @MS.new("\x04\x08i\x05"),
+      @MS.new("\x04\x08i\x06"),
+    ]
+    block = @MS.new "\x04\x080"
+
+    @statistics.add_message_send receiver, message, argv, block
+
+    assert_equal 1, @statistics.drb_message_sends
+  end
+
+  def test_add_result_receipt_exception
+    result = @MS.new "\x04\x08\"\x08FAIL" # not an exception
+
+    @statistics.add_result_receipt false, result
+
+    assert_equal 1, @statistics.drb_result_receipts
+    assert_equal 1, @statistics.drb_exceptions_raised
+  end
+
+  def test_add_result_receipt_success
+    result = @MS.new "\x04\x08\"\x06OK"
+
+    @statistics.add_result_receipt true, result
+
+    assert_equal 1, @statistics.drb_result_receipts
+    assert_equal 0, @statistics.drb_exceptions_raised
+  end
+
   def test_show_basic
     drbdump
 
